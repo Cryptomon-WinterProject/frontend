664e987342ad9483a9357e6e263968f8eeb11871
HarshilMendpara <mendparaharshil@gmail.com>
2022-02-05 16:27:24 +0530

    Created Landing Page
    Co-authored-by: Ujjawal gupta  <UjjawalGupta30@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from "react";
 
 import { Switch, Route } from "react-router-dom";
 
-import Home from "./Containers/Home";
+import LandingPage from "./Containers/LandingPage";
 
 const App = () => {
-  
-    return (
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-      </Switch>
-    )
-  
+  return (
+    <Switch>
+      <Route exact path="/">
+        <LandingPage />
+      </Route>
+    </Switch>
+  );
 };
 
 export default App;
