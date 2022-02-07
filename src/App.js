75e52ba99b77b1ac0913e388e38f4cdd43116167
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-02-07 14:59:53 +0530

    Added Routes for store and auction page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import LandingPage from "./Containers/LandingPage";
+import StorePage from "./Containers/StorePage/StorePage";
+import AuctionPage from "./Containers/AuctionPage/index";
 
 const App = () => {
   return (
     <Switch>
       <Route exact path="/">
         <LandingPage />
+      </Route>
+      <Route exact path="/store">
+        <StorePage />
+      </Route>
+      <Route exact path="/auction">
+        <AuctionPage />
       </Route>
     </Switch>
   );
