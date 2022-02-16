f344488529e5f5ca8eada7920f0a5fb19f19721d
Raj <varasaniraj5@gmail.com>
2022-02-16 22:48:02 +0530

    Updated popup paths

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUserCards, getUserData } from "./Services/user.service";
 import { useLocation } from "react-router-dom";
 import { getStoreCards } from "./Services/store.service";
-import PopUp from "./Components/Popup";
+import PopUp from "./Components/PopUp";
 import socketIo from "socket.io-client";
 import { SOCKET_URL } from "./Utils/constants";
 
