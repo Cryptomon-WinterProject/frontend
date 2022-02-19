fd7f57b19e7970d855139b1867b6ff9a69cff29a
Ujjawal gupta <imt_2020107@iiitm.ac.in>
2022-02-19 17:25:42 +0530

    Added heroku for uploading images

--- src/Services/photoUpload.service.js
+++ src/Services/photoUpload.service.js
@@ -4,7 +4,7 @@ import { UPLOAD_IMAGE_URL } from "../Utils/constants";
 export const uploadImage = async (images) => {
   try {
     const formData = new FormData();
-    formData.append("photo", images);
+    formData.append("photos", images);
     const { data } = await axios.post(UPLOAD_IMAGE_URL, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
