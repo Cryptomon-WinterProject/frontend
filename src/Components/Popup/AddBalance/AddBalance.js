41003667c511d8b2fff75fd9ff1733bcdf67e884
Akash Gupta <akash.gupta.anup@gmail.com>
2022-02-19 14:14:47 +0530

    Add Balance Pop
    Co-authored-by: Ujjawal Gupta <UjjawalGupta30@users.noreply.github.com>

--- src/Components/Popup/AddBalance/AddBalance.js
+++ src/Components/Popup/AddBalance/AddBalance.js
@@ -2,34 +2,50 @@ import React from "react";
 import styles from "./AddBalance.module.css";
 import cross from "../../../Assets/AddAuction/cross_black.svg";
 import moncoin from "../../../Assets/PlaceBid/moncoin.svg";
+import { useDispatch } from "react-redux";
 
 function AddBalance() {
+  const dispatch = useDispatch();
+
   return (
-    <div className={styles.MainWrapper}>
+    <form className={styles.MainWrapper}>
       <div className={styles.PopupWrapper}>
         <div className={styles.Topbar}>
           <div className={styles.Logo}>
             <span className={styles.yellow}>ADD </span>MONCOIN
           </div>
-          <img src={cross} alt="" className={styles.cross} />
+          <img
+            src={cross}
+            alt="cross"
+            className={styles.cross}
+            onClick={() => {
+              dispatch({
+                type: "HANDLE_POPUP_OPEN",
+                popupOpen: false,
+              });
+            }}
+          />
         </div>
         <div className={styles.BodyWrapper}>
           <div className={styles.Heading}>
-            Conversion rate :{" "}
+            Conversion rate :
             <span className={styles.yellow}>1000 GWEI/moncoin</span>
           </div>
           <div className={styles.Heading}>
-          <div className={styles.SubHeading}>Balance to add : </div><div className={styles.Button}>45
-            <img src={moncoin} alt="" className={styles.moncoin} /></div>
+            <div className={styles.SubHeading}>Balance to add : </div>
+            <div className={styles.Button}>
+              <input type="number" defaultValue={4} placeholder="Amount" />
+              <img src={moncoin} alt="" className={styles.moncoin} />
+            </div>
           </div>
 
-          <div className={styles.FinalButton}>
+          <button className={styles.FinalButton}>
             Pay 4500 GWEI
             <img src={moncoin} alt="" className={styles.moncoin} />
-          </div>
+          </button>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
 
